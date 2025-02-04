7d35e0545c969cb48ff11d29647f77b3204a2b36
Alifais Farrel Ramdhani <farrellabindonesia@gmail.com>
2025-02-04 00:10:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """Main UI for Kolosal Plane"""
-
+import pandas as pd
 import streamlit as st
 
 from interface.augmentation_interface import augmentation_interface
@@ -8,6 +8,16 @@ from interface.finetuning_interface import finetuning_interface
 st.set_page_config(layout="wide")
 
 st.title("Kolosal Plane Web Interface")
+
+# Load all session state
+if "llm" not in st.session_state:
+    st.session_state.llm = None
+if "slm" not in st.session_state:
+    st.session_state.slm = None
+if "thinking" not in st.session_state:
+    st.session_state.slm = None
+if "documents_df" not in st.session_state:
+    st.session_state.documents_df = pd.DataFrame(columns=["Documents"])
 
 tabs = st.tabs(["Data Augmentation", "LLM Finetuning"])
 
