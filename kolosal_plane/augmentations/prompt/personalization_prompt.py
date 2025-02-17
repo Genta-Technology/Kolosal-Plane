85dc6973ad4fd36f2aea75563d61283510bdc362
Alifais Farrel Ramdhani <farrellabindonesia@gmail.com>
2025-02-16 22:55:36 -0800

    simple finetuning by removing unnecessary file

--- kolosal_plane/augmentations/prompt/personalization_prompt.py
+++
@@ -1,8 +0,0 @@
-"""Default prompt for personalization dataset generation"""
-
-NEXT_QUESTION_PROMPT = """Based on this chat_history between the user and AI, your task is to generate a followup question that a user might ask
-{chat_history}
-
-Previous AI Response: {response}
-
-Your generated question might be related to the previous response or the chat history. You can also ask a question that is relevant to the context of the conversation."""
--- kolosal_plane/evaluation/evaluation_prompt.py
+++
@@ -1,7 +0,0 @@
-"""Default prompt for testing model"""
-
-TEST_QUESTION_PROMPT = """You are an AI that is tasked to generate questions for a conversation starter
-Relevant document: {document}
-
-Make sure you are generating question which is in the same language as the given document
-"""
