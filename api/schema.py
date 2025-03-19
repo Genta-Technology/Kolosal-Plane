2758a620806d0ae7c59bca085c9c5f19de6a01a6
Alifais Farrel Ramdhani <farrellabindonesia@gmail.com>
2025-03-19 10:02:36 -0700

    add schema for API

---
+++ api/schema.py
@@ -0,0 +1,42 @@
+"""API Schema for Kolosal Plane"""
+from typing import List, Optional, Dict, Literal
+from pydantic import BaseModel
+
+
+class LLMConfiguration(BaseModel):
+    """Configuration for Language Model"""
+    model_provider: Literal["azure", "openai",
+                            "genta", "fireworks", "anthropic"]
+    model_name: str
+    model_parameters: Optional[Dict] = {}
+    # API credentials
+    api_key: str
+    base_url: Optional[str] = None
+    api_version: Optional[str] = None
+
+
+class RequestEmbeddingAugmentation(BaseModel):
+    """Request start augmentation of knowledge based dataset augmentation"""
+    documents: List[str]
+    instruction: str
+    question_per_document: Optional[int] = 100
+    batch_size: Optional[int] = 10
+    llm_config: LLMConfiguration
+
+
+class RequestKnowledgeAugmentation(BaseModel):
+    """Request augmentation of knowledge based dataset"""
+    documents: List[str]
+    conversation_starter_instruction: str
+    conversation_personalization_instruction: str
+    system_prompt: str
+    conversation_starter_count: Optional[int] = 10
+    max_conversation_length: Optional[int] = 10
+    batch_size: Optional[int] = 16
+    llm_config: LLMConfiguration
+    tlm_config: Optional[LLMConfiguration] = None
+
+class ResponseAugmentation(BaseModel):
+    """Response of knowledge based dataset augmentation"""
+    generated_dataset: Dict
+    metadata: Dict[str, int]
